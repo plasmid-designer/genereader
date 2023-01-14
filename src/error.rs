96b6b9ab0e9b6cfb1e42da8b0fc4ce289b961ad2
SplittyDev <splittydev@gmail.com>
2023-01-14 01:22:44 +0100

    Expose file formats as features

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,9 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
+    #[cfg(feature = "fasta")]
     #[error("FASTA error: {0}")]
     FastaError(#[from] Box<crate::fasta::Error>),
+    #[cfg(feature = "fastq")]
     #[error("FASTQ error: {0}")]
     FastqError(#[from] Box<crate::fastq::Error>),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,37 @@
 mod api;
 mod error;
-mod fasta;
-mod fastq;
 mod helper;
 
+//
+// Internal exports
+//
 pub(crate) use helper::RuleExt;
+
+//
+// Public exports
+//
 
 pub use api::*;
 pub use error::{Error, Result};
 
+//
+// Public exports
+// Feature: fasta
+//
+
+#[cfg(feature = "fasta")]
+mod fasta;
+
+#[cfg(feature = "fasta")]
 pub use fasta::Fasta;
+
+//
+// Public exports
+// Feature: fastq
+//
+
+#[cfg(feature = "fastq")]
+mod fastq;
+
+#[cfg(feature = "fastq")]
 pub use fastq::Fastq;
